02f5484cfeff9b83a956379200b734e523a781eb
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-27 11:20:04 -0700

    updated migrations and seeds

--- data/migrations/20200427100522_create-tables.js
+++ data/migrations/20200427100522_create-tables.js
@@ -4,11 +4,15 @@ exports.up = function (knex) {
       tbl.increments();
       tbl.string("username").notNullable().unique();
       tbl.string("password", 100).notNullable();
+      tbl.text("bio");
     })
     .createTable("post", (tbl) => {
       tbl.increments();
+      tbl.string("title", 50).notNullable();
       tbl.string("img_url").notNullable();
+      tbl.string("location");
       tbl.text("story");
+      tbl.timestamp("created at").defaultTo(knex.fn.now());
       tbl
         .integer("user_id")
         .unsigned()
