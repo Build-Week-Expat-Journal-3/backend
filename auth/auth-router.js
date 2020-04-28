c106aa027e484bed2a023cc90f58f51caba6b095
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-28 13:22:24 -0700

    fixing login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -39,7 +39,7 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  db.getByUsername(username)
+  User.getByUsername(username)
     .then((user) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = createToken(user);
