c2ca1de78bba83f57fde0b436e85a5aca29d1eb7
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-29 15:25:39 -0700

    get by id and get user post endpoint testing

--- users/user-router.spec.js
+++ users/user-router.spec.js
@@ -16,8 +16,32 @@ describe("user-router.js", () => {
       expect(res.status).toBe(200);
     });
 
-    it("should return an array of users", async () => {
+    it("should return an array of 3 users", async () => {
       let res = await request(server).get("/api/users");
+      expect(res.body).toHaveLength(3);
+    });
+  });
+
+  describe("GET /api/users/:id", () => {
+    it("should return a status of 200", async () => {
+      let res = await request(server).get("/api/users/1");
+      expect(res.status).toBe(200);
+    });
+
+    it("should return the user object associated it ID in URL", async () => {
+      let res = await request(server).get("/api/users");
+      expect(res.body[0].username).toBe("jane");
+    });
+  });
+
+  describe("GET /api/users/:id/posts", () => {
+    it("should return a status of 200", async () => {
+      let res = await request(server).get("/api/users/1/posts");
+      expect(res.status).toBe(200);
+    });
+
+    it("should return an array of 3 posts", async () => {
+      let res = await request(server).get("/api/users/1/posts");
       expect(res.body).toHaveLength(3);
     });
   });
